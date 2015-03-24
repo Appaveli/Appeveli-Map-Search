2da879e9dfc40df10875b96fb4a2f7db3c46c7f0
Dominque Terry <terry_dominque@yahoo.com>
2015-03-24 09:00:09 -0400

    Appeveli Map Search
    This project uses the Map Search task to integrate search functionality
    in your windows phone 8 and silverlight 8.1 apps.

---
+++ AppeveliMapSearch.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Phone.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppeveliMapSearchTask
+{
+    class AppeveliMapSearch
+    {
+        public void SearchPlace(string search)
+        {
+            MapsTask mapsTask = new MapsTask();
+            mapsTask.SearchTerm = search;
+            mapsTask.ZoomLevel = 2;
+            mapsTask.Show();
+        }
+    }
+}
